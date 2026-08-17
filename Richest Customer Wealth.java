ae4b7bd4d2d5a570b5c3b1c4036f92f3d66e49cf
DANISHA-2007 <vtu30666@veltech.edu.in>
2026-08-17 11:14:39 +0530

    Add files via upload

---
+++ Richest Customer Wealth.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int wealth = 0;
+
+            for (int j = 0; j < accounts[i].length; j++) {
+                wealth += accounts[i][j];
+            }
+
+            maxWealth = Math.max(maxWealth, wealth);
+        }
+
+        return maxWealth;
+    }
+}
+
+OUTPUT:
+Accepted
+Runtime: 0 ms
+
+Case 1
+
+Case 2
+
+Case 3
+
+Input
+accounts =
+[[1,2,3],[3,2,1]]
+
+Output
+6
+
+Expected
+6
